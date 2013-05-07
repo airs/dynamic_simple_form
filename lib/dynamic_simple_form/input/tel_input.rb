7413409a4802592b006ab6818def7187b6905c70
Kenichi Tachibana <tachibana@airs.co.jp>
2013-05-07 18:27:57 +0900

    ソースコード内の日本語コメントは念のため削除

--- lib/dynamic_simple_form/input/tel_input.rb
+++ lib/dynamic_simple_form/input/tel_input.rb
@@ -4,7 +4,6 @@ module DynamicSimpleForm
   module Input
     class TelInput < StringBase
       def validate_string_value(field_value)
-        # 数字、スペース、+-()を許可し、+は先頭のみ、-が末尾に来ない
         unless /\A\+?[\d\(\) \-]*[\d\(\) ]\z/ =~ field_value.value
           field_value.errors.add(column, :invalid)
         end
